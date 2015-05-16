7f9fa450826583437ff087e77f260f1cff9eadfb
Jonathan Galsurkar <jgalsurkar@gmail.com>
2015-05-16 02:19:08 -0400

    Testing changes, need to get dot product to work

--- src/main.scala
+++ src/main.scala
@@ -17,27 +17,18 @@ object main {
     val lines = sc.textFile("project3.dat")
     
     val totalDocs = lines.count() //Counting the total number of documents
-    
-    type Document = (String, Int)
-    type term = String
-    type lineID = String
-    type totalWordCount = Int
-    type countInDoc = Int
-  
+      
     //Word Count
     val counts = lines.flatMap {
-                         line => lazy val s = line.split("\\s+")
-                         val totalCount = s.length
-                         val id = s.head
-                         
-                         s.filter(word => word.matches(gene)).map( word => ( (word: term, (id: lineID, totalCount: totalWordCount):Document ), 1: countInDoc) )//.map(word => ( (word: term, ((id: lineID, totalCount: totalWordCount):Document , 1: countInDoc) )))
+                         line => lazy val words = line.split("\\s+")
+                         words.filter(term => term.matches(gene)).map(term => ( (term, words.head, words.length ), 1) )//.map(word => ( (word: term, ((id: lineID, totalCount: totalWordCount):Document , 1: countInDoc) )))
                    }.reduceByKey(_+_)
     //COUNTS CONTAINS (term, documentID, totalDocumentWordCount) => wordCountForTerm
 
-    val counts2 = counts.map( quad => ( quad._1._1, ((quad._1._2._1, quad._1._2._2): Document, quad._2 )))
+    val counts2 = counts.map( quad => ( quad._1._1, (quad._1._2, quad._1._3, quad._2 )))
     //COUNT2 REMAPS COUNTS AND CONTAINS term => (documentID, totalDocumentWordCount, wordCountForTerm)
     
-    val counts3 = counts2.groupByKey().mapValues(array => array.map(pair => (pair._1._1, (pair._2/pair._1._2.toDouble) * log(totalDocs/array.size))))
+    val counts3 = counts2.groupByKey().mapValues(array => array.map(pair => (pair._1, (pair._3/pair._2.toDouble) * log(totalDocs/array.size))))
     //COUNTS3 GROUPS COUNTS2 BY KEY, calculates tf/idf and CONTAINS term => List[documentID, tf/idf]
     
    // counts3.sortBy((a=> a._1),true, 10).foreach(x => println(x))
@@ -46,64 +37,66 @@ object main {
                  //.map(pair => ((pair._1._1,pair._2._1) , cosSimilarity(pair._1._2.toList,pair._2._2.toList) ))
    //FILTER BASED ON LEXICOGRAPHICAL DIFFERENCE TO GET ALL COMBINATIONS
    //COUNTS4 CONTAINS ALL OF THE PAIRS WHICH WE MUST CALCULATE THE COSINE SIMILARITY
-   //counts4.foreach(x => println(x))
    
-   val counts5 = counts4.map(pair => ((pair._1._1,pair._2._1) , cosSimilarity(pair._1._2.toList,pair._2._2.toList) ))
-   counts5.foreach(x => println(x))
+   val withNorms = counts4.map(pair => ((pair._1._1,pair._2._1), productOfNorms(pair._1._2,pair._2._2)))
+  // withNorms.foreach(x=>println(x))
    
-   //val list1: List[Tuple2[String,Double]] =  List( ("a",3), ("b",2), ("d",5), ("e",5) )
-   //val list2: List[Tuple2[String,Double]] =  List( ("a",2), ("c",2), ("e",7) )
+   val withDotProduct =  counts4.map(pair => ((pair._1._1,pair._2._1), dotProduct(pair._1._2,pair._2._2)))
+    //withDotProduct.foreach(x=>println(x))
    
-   //val x = cosSimilarity(list1,list2)
-                   
+   val list1: Iterable[(String,Double)] =  Iterable( ("a",3), ("b",2), ("d",5), ("e",5) )
+   val list2: Iterable[(String,Double)] =  Iterable( ("a",2), ("c",2), ("e",7) )
+   val x = dotProduct(list1,list2)
+   println(x)
+   
+    /* (TERM, TERM)
+   val c4 = counts4.map(pair => (pair._1._1, pair._2._1))
+      
+   val normsA = counts4.keys.values.map(x => pow(x.map(y=> pow(y._2, 2)).sum, .5))
+   val normsB = counts4.values.values.map(x => pow(x.map(y=> pow(y._2, 2)).sum, .5))
+  
+   val counts5 = counts4.keys.values.flatMap(f => f)
+   val counts6 = counts4.values.values.flatMap(f => f)
+   
+   val dotProduct = counts5.cogroup(counts6).filter(vals => vals._2._1.size > 0 && vals._2._2.size > 0).map(a => (a._2._1.head * a._2._2.head)).sum
+  
+   //val c8 = dotProduct.zip(c4)
+   */
+   /*
+   
+   val counts5 = counts4.map(pair => ((pair._1._1,pair._2._1) , (pair._1._2,pair._2._2) )).collectAsMap()
+  // l.foreach(x => println("hello"))
+   
+   //val cos = l.map(lists => (cosSimilarity(lists._1,lists._2)))
+   //cos.foreach(x => println(x))
+  */        
+  }
+  
+  def productOfNorms(A:Iterable[(String,Double)],B:Iterable[(String,Double)]): Double = {
+    getNorm(A) * getNorm(B)
+  }
+  
+  def getNorm(vector: Iterable[(String,Double)]): Double = {
+    pow(vector.map(x => pow(x._2, 2)).sum, .5)
   }
 
+  def dotProduct(A:Iterable[(String,Double)],B:Iterable[(String,Double)]): Double = {
 
-  def cosSimilarity(A:List[(String,Double)],B:List[(String,Double)]): Double = {//RDD[Tuple2[String,Double]] = {
-    println("near, far, wherever you are")
-    println("near, far, wherever you are")
-    println("near, far, wherever you are")
-    println("near, far, wherever you are")
-    println("near, far, wherever you are")
-    println("near, far, wherever you are")
-    println("near, far, wherever you are")
-    println("near, far, wherever you are")
-    println("near, far, wherever you are")
-    println("near, far, wherever you are")
-    println("near, far, wherever you are")
-    println("near, far, wherever you are")
-    println("near, far, wherever you are")
-    println("near, far, wherever you are")
-    println("near, far, wherever you are")
-    println("near, far, wherever you are")
-    println("near, far, wherever you are")
-    println("near, far, wherever you are")
-    println("near, far, wherever you are")
-    
-    val vecA = sc.parallelize(A)//.map(pair => (pair._1,pair._2))
-      val vecB = sc.parallelize(B)//.map(pair => (pair._1,pair._2))
-      
-      val normA = pow(vecA.map(x => pow(x._2, 2)).sum , .5)
-      val normB = pow(vecB.map(x => pow(x._2, 2)).sum , .5)
-      
-      //val dotProduct = vecA.union(vecB).reduceByKey(_*_)//.map(pair =>(pair._1,pair._2)).reduceByKey(_*_)
-     // dotProduct.foreach(x => println(x))
-      
-      val dotProduct = vecA.cogroup(vecB).filter{
+      val dotProduct = (A ++ B).groupBy(_._1).mapValues(_.map(_._2)).filter{
+        
+        case(vals) => (vals._2.size > 1)
+        
+      }
+      .map(x=>x._2.product)
+      /*val dotProduct = A.cogroup(B).filter{
         case(vals) => (vals._2._1.size > 0 && vals._2._2.size > 0)
       }.map(a => (a._2._1.head * a._2._2.head))
       
-      dotProduct.foreach(x => println(x))
       
-      (dotProduct.sum / (normA * normB))
-  
-  
+*/
+      dotProduct.foreach(x=>println(x))
+      return dotProduct.sum
+   
   }
 
 }
-
-
-
-
-
-
